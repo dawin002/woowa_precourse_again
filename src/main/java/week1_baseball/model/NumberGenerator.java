22ac52ea545da923762ccb3fab894d5917fb5334
Dawin <dawin002@naver.com>
2023-11-29 18:13:35 +0900

    feat(NumberGenerator): 랜덤 숫자 생성 기능 추가
    랜덤 숫자 생성 기능:
    - int createNumber()
    - 1~9 범위의 랜덤 숫자 int로 반환

---
+++ src/main/java/week1_baseball/model/NumberGenerator.java
@@ -0,0 +1,11 @@
+package week1_baseball.model;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+public class NumberGenerator {
+    private static final int NUMBER_RANGE_START = 1;
+    private static final int NUMBER_RANGE_END = 9;
+    public static int createNumber() {
+        return Randoms.pickNumberInRange(NUMBER_RANGE_START, NUMBER_RANGE_END);
+    }
+}
