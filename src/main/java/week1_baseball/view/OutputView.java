e6bbfe7cff3f9002f69f8d1b9a14640863c13bc5
Dawin <dawin002@naver.com>
2023-11-29 17:58:36 +0900

    feat(OutputView): 게임 시작 메시지 출력 기능 추가
    게임 시작 메시지 출력 기능:
    - void printGameStartMessage()
    - "숫자 야구 게임을 시작합니다." 출력

---
+++ src/main/java/week1_baseball/view/OutputView.java
@@ -0,0 +1,8 @@
+package week1_baseball.view;
+
+public class OutputView {
+    private static final String GAME_START_MESSAGE = "숫자 야구 게임을 시작합니다.";
+    public void printGameStartMessage() {
+        System.out.println(GAME_START_MESSAGE);
+    }
+}
