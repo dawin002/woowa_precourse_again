79c2393cc9ad17893f3ff34cdbc4aa4990f9b017
Dawin <dawin002@naver.com>
2023-11-30 19:13:32 +0900

    test(PlayerNumbersTest): 플레이어 숫자 설정 기능 테스트
    전달된 정수 리스트로 플레이어 숫자 객체 초기화:
    - setPlayerNumbersByGivenNumbers()
    - 전달된 리스트로 초기화된 플레이어 숫자 객체인지 검증
    숫자가 3개가 아닌 경우 예외 처리:
    - validateNumbersSize()
    - 예외 조건 검사 함수 호출
    - 숫자가 3개가 아닌 경우 예외가 발생하는지 검증
    숫자 중 0이 포함된 경우 예외 처리:
    - validateNumbersHasZero()
    - 숫자에 0이 포함되어 있는 경우 예외가 발생하는지 검증
    중복된 숫자가 존재하는 경우 예외 처리:
    - validateNumbersDuplicated()
    - 중복된 숫자가 있는 경우 예외가 발생하는지 검증

---
+++ src/test/java/week1_baseball/model/PlayerNumbersTest.java
@@ -0,0 +1,64 @@
+package week1_baseball.model;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class PlayerNumbersTest {
+
+    @DisplayName("전달된 정수 리스트로 플레이어 숫자 객체 초기화")
+    @Test
+    void setPlayerNumbersByGivenNumbers() {
+        // given
+        List<Integer> numbers = List.of(1, 2, 3);
+
+        // when
+        PlayerNumbers playerNumbers = new PlayerNumbers(numbers);
+
+        // then
+        for (int number : numbers) {
+            assertThat(playerNumbers.getNumbers()).contains(number);
+        }
+    }
+
+    @DisplayName("숫자가 3개가 아닌 경우 예외 처리")
+    @Test
+    void validateNumbersSize() {
+        // given
+        List<Integer> numbers = List.of(1, 2);
+
+        // when then
+        assertThatThrownBy(() -> new PlayerNumbers(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("3자리 숫자가 아닙니다.");
+    }
+
+    @DisplayName("숫자 중 0이 포함된 경우 예외 처리")
+    @Test
+    void validateNumbersHasZero() {
+        // given
+        List<Integer> numbers = List.of(0, 1, 2);
+
+        // when then
+        assertThatThrownBy(() -> new PlayerNumbers(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("1~9 사이가 아닌 숫자가 포함되었습니다.");
+    }
+
+    @DisplayName("중복된 숫자가 존재하는 경우 예외 처리")
+    @Test
+    void validateNumbersDuplicated() {
+        // given
+        List<Integer> numbers = List.of(1, 1, 2);
+
+        // when then
+        assertThatThrownBy(() -> new PlayerNumbers(numbers))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessageContaining("중복된 숫자가 있습니다.");
+    }
+
+}
