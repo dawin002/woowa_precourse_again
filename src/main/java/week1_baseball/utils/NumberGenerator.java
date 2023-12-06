ecc5631dc5e7a90b25aedfe0d82f384815d20d60
Dawin <dawin002@naver.com>
2023-12-06 14:52:30 +0900

    refactor(NumberGenerator): 랜덤 숫자 생성 기능 수정
    랜덤 숫자 생성 기능 수정:
    - createUniqueNumbers() 함수 추가
    - 1~9 사이의 중복되지 않은 3개의 숫자 리스트 반환

--- src/main/java/week1_baseball/utils/NumberGenerator.java
+++ src/main/java/week1_baseball/utils/NumberGenerator.java
@@ -2,10 +2,25 @@ package week1_baseball.utils;
 
 import camp.nextstep.edu.missionutils.Randoms;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class NumberGenerator {
+    private static final int NUMBERS_SIZE = 3;
     private static final int NUMBER_RANGE_START = 1;
     private static final int NUMBER_RANGE_END = 9;
-    public static int createNumber() {
+
+    public static List<Integer> createUniqueNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+        while (numbers.size() < NUMBERS_SIZE) {
+            int randomNumber = NumberGenerator.createNumber();
+            if (!numbers.contains(randomNumber)) {
+                numbers.add(randomNumber);
+            }
+        }
+        return numbers;
+    }
+    private static int createNumber() {
         return Randoms.pickNumberInRange(NUMBER_RANGE_START, NUMBER_RANGE_END);
     }
 }
--- src/test/java/week1_baseball/utils/NumberGeneratorTest.java
+++ src/test/java/week1_baseball/utils/NumberGeneratorTest.java
@@ -5,30 +5,31 @@ import org.junit.jupiter.api.Test;
 import week1_baseball.utils.NumberGenerator;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 class NumberGeneratorTest {
-
+    private static final int NUMBERS_SIZE = 3;
     private static final int NUMBER_RANGE_START = 1;
     private static final int NUMBER_RANGE_END = 9;
 
-    @DisplayName("1~9 범위의 랜덤 숫자 생성")
+    @DisplayName("1~9 범위의 중복되지 않은 3개의 숫자 생성")
     @Test
-    void createRandomNumberInRangeFrom1To9() {
+    void createRandomNumbers() {
         // given
-        List<Integer> numbers = new ArrayList<>();
 
         // when
-        for (int i = 0; i < 100; i++) {
-            numbers.add(NumberGenerator.createNumber());
-        }
+        List<Integer> numbers = NumberGenerator.createUniqueNumbers();
 
         // then
+        assertThat(numbers.size()).isEqualTo(NUMBERS_SIZE);
         for (int number : numbers) {
             assertThat(number).isGreaterThanOrEqualTo(NUMBER_RANGE_START);
             assertThat(number).isLessThanOrEqualTo(NUMBER_RANGE_END);
         }
+        HashSet<Integer> numberSet = new HashSet<>(numbers);
+        assertThat(numberSet.size()).isEqualTo(NUMBERS_SIZE);
     }
 }
