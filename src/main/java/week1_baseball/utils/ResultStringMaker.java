3f776bfa222e3289e7328c7da0ccfabff40b3888
Dawin <dawin002@naver.com>
2023-12-06 20:47:06 +0900

    feat(ResultStringMaker): 게임 결과 메시지 생성 기능 추가
    게임 결과 메시지 생성 기능:
    - makeString()
    - 게임 결과 Dto를 전달받아 결과 메시지 생성 후 반환

---
+++ src/main/java/week1_baseball/utils/ResultStringMaker.java
@@ -0,0 +1,52 @@
+package week1_baseball.utils;
+
+import week1_baseball.dto.BaseballGameResult;
+
+import static week1_baseball.message.ErrorMessages.NO_MATCH_RESULT_ERROR;
+import static week1_baseball.message.ResultMessages.*;
+
+public class ResultStringMaker {
+    public static String makeString(BaseballGameResult gameResult) {
+        if (gameResult.getStrike() == 0 && gameResult.getBall() == 0) {
+            return getNothingMessage();
+        }
+        if (gameResult.getStrike() != 0 && gameResult.getBall() != 0) {
+            return getStrikeAndBallMessage(gameResult);
+        }
+        if (gameResult.getStrike() != 0 && gameResult.getBall() == 0) {
+            return getOnlyStrikeMessage(gameResult);
+        }
+        if (gameResult.getStrike() == 0 && gameResult.getBall() != 0) {
+            return getOnlyBallMessage(gameResult);
+        }
+        throw new IllegalStateException(NO_MATCH_RESULT_ERROR.getMessage());
+    }
+
+    private static String getNothingMessage() {
+        return NOTHING.getMessage();
+    }
+
+    private static String getStrikeAndBallMessage(BaseballGameResult gameResult) {
+        return new StringBuilder()
+                .append(gameResult.getBall())
+                .append(BALL.getMessage())
+                .append(SPACE.getMessage())
+                .append(gameResult.getStrike())
+                .append(STRIKE.getMessage())
+                .toString();
+    }
+
+    private static String getOnlyStrikeMessage(BaseballGameResult gameResult) {
+        return new StringBuilder()
+                .append(gameResult.getStrike())
+                .append(STRIKE.getMessage())
+                .toString();
+    }
+
+    private static String getOnlyBallMessage(BaseballGameResult gameResult) {
+        return new StringBuilder()
+                .append(gameResult.getBall())
+                .append(BALL.getMessage())
+                .toString();
+    }
+}
