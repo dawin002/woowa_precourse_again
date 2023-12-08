b140bb2d0c4d25579be70de105e6809e4fb6de77
Dawin <dawin002@naver.com>
2023-12-08 18:59:10 +0900

    feat(InputView): 입력 기능 추가
    플레이어 숫자 입력 기능:
    - readPlayerNumbers()
    - "숫자를 입력해주세요 : " 줄바꿈 없이 출력
    - 문자열 입력받아 반환
    게임 재시작 숫자 입력 기능:
    - readReplayNumber()
    - "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요." 출력
    - 문자열 입력받아 반환

--- src/main/java/week1_baseball/view/InputView.java
+++ src/main/java/week1_baseball/view/InputView.java
@@ -2,12 +2,16 @@ package week1_baseball.view;
 
 import camp.nextstep.edu.missionutils.Console;
 
-import java.util.List;
-
 public class InputView {
-    private static final String MESSAGE_READ_PLAYER_NUMBERS = "숫자를 입력해주세요 : ";
+    private static final String READ_PLAYER_NUMBERS_MESSAGE = "숫자를 입력해주세요 : ";
+    private static final String READ_REPLAY_NUMBER_MESSAGE = "게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.";
     public String readPlayerNumbers() {
-        System.out.print(MESSAGE_READ_PLAYER_NUMBERS);
+        System.out.print(READ_PLAYER_NUMBERS_MESSAGE);
+        return Console.readLine();
+    }
+
+    public String readReplayNumber() {
+        System.out.println(READ_REPLAY_NUMBER_MESSAGE);
         return Console.readLine();
     }
 }
