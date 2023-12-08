687dfc08601ea483cf8793d1efadb9ad8fcfcc09
Dawin <dawin002@naver.com>
2023-12-08 19:56:25 +0900

    refactor(ErrorMessage): 에러 메시지 enum 수정
    에러 위치에 따른 메시지 반환 함수를 getMessage() 함수로 통합:
    - getComputerMessage()
    - getPlayerMessage()
    - getRestartMessage()
    - getMessage()
    에러 위치에 따른 에러 타이틀 반환 함수 생성:
    - getTitle()

--- src/main/java/week1_baseball/message/ErrorMessage.java
+++ src/main/java/week1_baseball/message/ErrorMessage.java
@@ -2,13 +2,17 @@ package week1_baseball.message;
 
 public enum ErrorMessage {
     ERROR_HEADER("[ERROR] "),
-    COMPUTER_TITLE("정답 숫자"),
-    PLAYER_TITLE("입력된 숫자"),
-    RESTART_TITLE("게임 재시작 숫자"),
+    INPUT_ERROR_TITLE("입력된 값"),
+    COMPUTER_ERROR_TITLE("정답 숫자"),
+    PLAYER_ERROR_TITLE("입력된 숫자"),
+    RESULT_ERROR_TITLE("계산 결과"),
+    RESTART_ERROR_TITLE("게임 재시작 숫자"),
+    EMPTY_INPUT_ERROR_MESSAGE("이 비어 있습니다."),
+    CAN_NOT_PARSE_INT_ERROR_MESSAGE("에 숫자가 아닌 값이 있습니다."),
     WRONG_SIZE_ERROR("가 3자리 숫자가 아닙니다."),
     HAS_ZERO_ERROR("에 1~9 사이가 아닌 숫자가 포함되었습니다."),
     DUPLICATED_ERROR("에 중복된 숫자가 존재합니다."),
-    NO_MATCH_RESULT_ERROR("해당하는 조건의 결과가 없습니다."),
+    NO_MATCH_RESULT_ERROR("에 일치하는 조건의 결과가 없습니다."),
     WRONG_RESTART_NUMBER_ERROR("가 1 또는 2가 아닙니다.")
     ;
 
@@ -18,19 +22,11 @@ public enum ErrorMessage {
         this.message = message;
     }
 
-    public String getComputerMessage() {
-        return String.format(ERROR_HEADER.message, COMPUTER_TITLE.message, this.message);
-    }
-
-    public String getPlayerMessage() {
-        return String.format(ERROR_HEADER.message, PLAYER_TITLE.message, this.message);
-    }
-
-    public String getRestartMessage() {
-        return String.format(ERROR_HEADER.message, RESTART_TITLE.message, this.message);
+    public String getTitle() {
+        return String.format(ERROR_HEADER.message, this.message);
     }
 
     public String getMessage() {
-        return String.format(ERROR_HEADER.message, this.message);
+        return String.format(this.message);
     }
 }
