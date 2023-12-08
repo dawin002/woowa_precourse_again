3092ee9550430bff026f84a714e72d157096827e
Dawin <dawin002@naver.com>
2023-12-08 20:33:44 +0900

    refactor(InputParserTest): 입력값 변환 기능 테스트 수정
    문자열을 정수 리스트로 변환해 반환:
    - parseStringToIntList()
    정수 리스트로 변환할 문자열이 비어있는 경우 예외 처리:
    - validateInputEmptyForParseIntList()
    정수 리스트로 변환할 문자열을 정수로 변환할 수 없는 경우 예외 처리:
    - validateInputNotNumberForParseIntList()
    전달된 문자열을 정수로 변환해 반환:
    - parseStringToInt()
    정수로 변환할 문자열이 비어있는 경우 예외 처리:
    - validateInputEmptyForParseInt()
    정수로 변환할 문자열을 정수로 변환할 수 없는 경우 예외 처리:
    - validateInputNotNumberForParseInt()

--- src/test/java/week1_baseball/utils/InputParserTest.java
+++ src/test/java/week1_baseball/utils/InputParserTest.java
@@ -11,7 +11,7 @@ import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class InputParserTest {
 
-    @DisplayName("전달된 문자열을 정수 리스트로 변환해 반환")
+    @DisplayName("문자열을 정수 리스트로 변환해 반환")
     @ValueSource(strings = {"123", "246", "999"})
     @ParameterizedTest
     void parseStringToIntList(String input) {
@@ -25,24 +25,55 @@ class InputParserTest {
         }
     }
 
-    @DisplayName("문자열이 비어있는 경우 예외 처리")
+    @DisplayName("정수 리스트로 변환할 문자열이 비어있는 경우 예외 처리")
     @ValueSource(strings = {"", " ", "   "})
     @ParameterizedTest
-    void validateInputEmpty(String input) {
+    void validateInputEmptyForParseIntList(String input) {
         // given when then
         assertThatThrownBy(() -> InputParser.stringToIntList(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("입력된 값이 비어 있습니다.");
+                .hasMessage("[ERROR] 입력된 값이 비어 있습니다.");
     }
 
-    @DisplayName("문자열을 정수로 변환할 수 없는 경우 예외 처리")
+    @DisplayName("정수 리스트로 변환할 문자열을 정수로 변환할 수 없는 경우 예외 처리")
     @ValueSource(strings = {"-123", "0.512", "12!", "13a"})
     @ParameterizedTest
-    void validateInputCanNotParseInteger(String input) {
+    void validateInputNotNumberForParseIntList(String input) {
         // given when then
         assertThatThrownBy(() -> InputParser.stringToIntList(input))
                 .isInstanceOf(IllegalArgumentException.class)
-                .hasMessage("입력된 값에 숫자가 아닌 값이 있습니다.");
+                .hasMessage("[ERROR] 입력된 값에 숫자가 아닌 값이 있습니다.");
+    }
+
+    @DisplayName("전달된 문자열을 정수로 변환해 반환")
+    @ValueSource(strings = {"1", "2", "0"})
+    @ParameterizedTest
+    void parseStringToInt(String input) {
+        // given when
+        int number = InputParser.stringToInt(input);
+
+        // then
+        assertThat(number).isEqualTo(Integer.parseInt(input));
+    }
+
+    @DisplayName("정수로 변환할 문자열이 비어있는 경우 예외 처리")
+    @ValueSource(strings = {"", " ", "   "})
+    @ParameterizedTest
+    void validateInputEmptyForParseInt(String input) {
+        // given when then
+        assertThatThrownBy(() -> InputParser.stringToInt(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력된 값이 비어 있습니다.");
+    }
+
+    @DisplayName("정수로 변환할 문자열을 정수로 변환할 수 없는 경우 예외 처리")
+    @ValueSource(strings = {"-123", "0.512", "12!", "13a"})
+    @ParameterizedTest
+    void validateInputNotNumberForParseInt(String input) {
+        // given when then
+        assertThatThrownBy(() -> InputParser.stringToInt(input))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("[ERROR] 입력된 값에 숫자가 아닌 값이 있습니다.");
     }
 
 }
