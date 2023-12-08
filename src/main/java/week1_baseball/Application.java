25891b6eb0b6c9bba7e9e7d98db083a099f0203c
Dawin <dawin002@naver.com>
2023-12-08 21:19:38 +0900

    feat(Application): 어플리케이션 실행 코드 추가
    객체 생성:
    - InputView
    - OutputView
    - BaseballGameService
    - BaseballController
    게임 실행 코드:
    - baseballController.startGame()

--- src/main/java/week1_baseball/Application.java
+++ src/main/java/week1_baseball/Application.java
@@ -1,7 +1,17 @@
 package week1_baseball;
 
+import week1_baseball.controller.BaseballController;
+import week1_baseball.service.BaseballGameService;
+import week1_baseball.view.InputView;
+import week1_baseball.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        System.out.println("asdf");
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        BaseballGameService gameService = new BaseballGameService();
+
+        BaseballController baseballController = new BaseballController(inputView, outputView, gameService);
+        baseballController.startGame();
     }
 }
