c08081c557450dd3c26e65f9234bd672728050e1
Dawin <dawin002@naver.com>
2023-12-09 19:55:57 +0900

    test(RaceServiceTest): 자동차 리스트 초기화 테스트 추가
    전달된 이름으로 초기화한 자동차 리스트 생성:
    - createCarsWithGivenNames()
    이동 거리를 0으로 초기화한 자동차 리스트 생성:
    - createCarsWithDefaultDistance()
    중복된 자동차 이름이 존재하는 경우 예외 처리:
    - validateCarNamesNotDuplicated()

--- src/test/java/week2_racing_car/service/RaceServiceTest.java
+++ src/test/java/week2_racing_car/service/RaceServiceTest.java
@@ -3,11 +3,13 @@ package week2_racing_car.service;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import week2_racing_car.dto.CarResult;
+import week2_racing_car.dto.SingleRaceResult;
 
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class RaceServiceTest {
     private RaceService raceService;
@@ -27,7 +29,41 @@ class RaceServiceTest {
         raceService.setCars(carNames);
 
         // then
-//        assertThat()
+        SingleRaceResult raceResult = raceService.getResult();
+        List<CarResult> carResults = raceResult.getResults();
+
+        for (int i = 0; i < carResults.size(); i++) {
+            assertThat(carResults.get(i).getName()).isEqualTo(carNames.get(i));
+        }
+    }
+
+    @DisplayName("이동 거리를 0으로 초기화한 자동차 리스트 생성")
+    @Test
+    void createCarsWithDefaultDistance() {
+        // given
+        List<String> carNames = List.of("a", "b", "c", "d");
+
+        // when
+        raceService.setCars(carNames);
+
+        // then
+        SingleRaceResult raceResult = raceService.getResult();
+
+        for (CarResult carResult : raceResult.getResults()) {
+            assertThat(carResult.getDistance()).isEqualTo(0);
+        }
+    }
+
+    @DisplayName("중복된 자동차 이름이 존재하는 경우 예외 처리")
+    @Test
+    void validateCarNamesNotDuplicated() {
+        // given
+        List<String> carNames = List.of("a", "b", "c", "a");
+
+        // when then
+        assertThatThrownBy(() -> raceService.setCars(carNames))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("중복된 자동차 이름은 입력할 수 없습니다.");
     }
 
 }
